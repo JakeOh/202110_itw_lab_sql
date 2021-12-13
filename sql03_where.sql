a45bbcaec3a5e90432a2dbe45eda5f4dce8210ca
JakeOh <jake.oh.lecture@gmail.com>
2021-12-13 17:29:30 +0900

    Add files via upload

--- sql03_where.sql
+++ sql03_where.sql
@@ -37,3 +37,72 @@ select empno, ename, job, sal
 from emp
 where sal between 2000 and 3000
 order by sal desc;
+
+-- 직원 테이블에서 10번 또는 20번 부서에서 근무하는 직원들의 부서번호, 이름, 급여를 검색
+-- 출력 순서는 (1) 부서 번호 (2) 이름 오름차순 정렬.
+select deptno, ename, sal
+from emp
+where deptno = 10 or deptno = 20
+order by deptno, ename;
+
+select deptno, ename, sal
+from emp
+where deptno in (10, 20)
+order by deptno, ename;
+
+-- SQL에서는 숫자만이 아니라 문자열을 비교할 때도 비교 연산자(=, !=, >, ...)을 사용함.
+
+-- 직원 테이블에서 직책이 'CLERK'인 직원들의 직책, 이름, 급여를 출력.
+-- 이름 오름차순 순서로 출력.
+select job, ename, sal
+from emp
+where job = 'CLERK'
+order by ename;
+
+-- SQL에서 문자열은 작은따옴표('')를 사용.
+-- 테이블에 저장된 데이터(문자열)은 대/소문자를 구분.
+
+-- 직원 테이블에서 직책이 CLERK 또는 MANAGER인 직원들의 직책, 이름, 급여를 검색.
+-- 출력 순서 (1) 직책 (2) 급여 오름차순 정렬.
+select job, ename, sal
+from emp
+where job = 'CLERK' or job = 'MANAGER'
+order by job, sal;
+
+select job, ename, sal
+from emp
+where job in ('CLERK', 'MANAGER')
+order by job, sal;
+
+-- 직원 테이블에서 20번 부서에서 근무하는 CLERK의 모든 정보를 검색
+select * from emp
+where deptno = 20 and job = 'CLERK';
+
+-- 직원 테이블에서 20번 부서에서 근무하거나 또는 직책이 CLERK인 직원들의 모든 정보를 검색
+select * from emp
+where deptno = 20 or job = 'CLERK';
+
+-- 직원 테이블에서 CLERK, ANALYST, MANAGER가 아닌 직원들의 사번, 이름, 직책, 급여를 검색
+-- 사번 오름차순 정렬.
+-- (1)
+select empno, ename, job, sal
+from emp
+where job != 'CLERK' and job != 'ANALYST' and job != 'MANAGER'
+order by empno;
+
+-- (2)
+select empno, ename, job, sal
+from emp
+where not (job = 'CLERK' or job = 'ANALYST' or job = 'MANAGER')
+order by empno;
+
+-- (3)
+select empno, ename, job, sal
+from emp
+where not job in ('CLERK', 'ANALYST', 'MANAGER')
+order by empno;
+
+select empno, ename, job, sal
+from emp
+where job not in ('CLERK', 'ANALYST', 'MANAGER')
+order by empno;
