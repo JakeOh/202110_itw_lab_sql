62c730386ee14649ebd4506390a7601323d4951c
JakeOh <jake.oh.lecture@gmail.com>
2021-12-15 10:20:07 +0900

    Add files via upload

--- sql07_subquery.sql
+++ sql07_subquery.sql
@@ -13,18 +13,65 @@ where sal > (
 );
 
 -- ALLEN보다 적은 급여를 받는 직원들의 사번, 이름, 급여를 검색
+select empno, ename, sal
+from emp
+where sal < (
+    select sal from emp where ename = 'ALLEN'
+);
 
 -- ALLEN과 같은 직책의 직원들의 사번, 이름, 부서번호, 직책, 급여를 검색
+select empno, ename, deptno, job, sal
+from emp
+where job = (
+    select job from emp where ename = 'ALLEN'
+);
 
 -- SALESMAN의 급여 최댓값보다 더 많은 급여를 받는 직원들의 사번, 이름, 급여, 직책을 검색
+select empno, ename, sal, job
+from emp
+where sal > (
+    select max(sal) from emp where job = 'SALESMAN'
+);
 
 -- 연봉 = sal * 12 + comm. 만약 comm이 null이면 0으로 계산.
 -- WARD의 연봉보다 더 많은 연봉을 받는 직원들의 사번, 이름, 급여, 수당, 연봉을 검색.
 -- 연봉 내림차순으로 출력.
+select sal * 12 + nvl(comm, 0) 
+from emp
+where ename = 'WARD';
+
+select empno, ename, sal, comm,
+    sal * 12 + nvl(comm, 0) as ANNUAL_SAL
+from emp
+where sal * 12 + nvl(comm, 0) > (
+    select sal * 12 + nvl(comm, 0) 
+    from emp
+    where ename = 'WARD'
+);
 
 -- 각 부서에서 급여가 가장 적은 직원들의 레코드를 검색
+select deptno, min(sal)
+from emp
+group by deptno;
+
+-- 단일행 서브쿼리: 서브쿼리의 결과가 행이 1개인 경우.
+-- 다중행 서브쿼리: 서브쿼리의 결과가 행이 2개 이상인 경우.
+--   한 개의 값과 단순 비교(=, !=, >, <, ...)를 할 수 없음.
+--   in, any, all과 같은 키워드를 함께 사용.
+
+select * 
+from emp
+where (deptno, sal) in (
+    select deptno, min(sal) from emp group by deptno
+);
 
 -- 각 부서에서 급여가 가장 많은 직원들의 레코드를 검색
+select *
+from emp
+where (deptno, sal) in (
+    select deptno, max(sal) from emp
+    group by deptno
+);
 
 -- SCOTT과 같은 급여를 받는 직원들의 이름과 급여를 검색
 
