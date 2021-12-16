09ea9665bc81a9a252fbfab8c59c1b472c4b4d32
JakeOh <jake.oh.lecture@gmail.com>
2021-12-16 11:20:58 +0900

    Add files via upload

--- sql09_exercises.sql
+++ sql09_exercises.sql
@@ -44,20 +44,73 @@ from employees
 where to_char(hire_date, 'YYYY') = '2008';
 
 -- 7. 2008년에 입사한 직원들의 부서 이름과 부서별 인원수 검색.
+select d.department_name, count(*)
+from departments d join employees e
+    on d.department_id = e.department_id
+where to_char(e.hire_date, 'YYYY') = '2008'
+group by d.department_name;
 
+select d.department_name, count(*)
+from departments d, employees e
+where d.department_id = e.department_id
+    and to_char(e.hire_date, 'YYYY') = '2008'
+group by d.department_name;
 
 -- 8. 2008년에 입사한 직원들의 부서 이름과 부서별 인원수 검색. 
 -- 단, 부서별 인원수가 5명 이상인 경우만 출력.
+select d.department_name, count(*)
+from departments d join employees e
+    on d.department_id = e.department_id
+where to_char(e.hire_date, 'YYYY') = '2008'
+group by d.department_name
+having count(*) >= 5;
 
 -- 9. 부서번호, 부서별 급여 평균을 검색. 소숫점 한자리까지 반올림 출력.
+select department_id, round(avg(salary), 1) as AVG_SAL
+from employees
+group by department_id
+order by department_id;
 
 -- 10. 부서별 급여 평균이 최대인 부서의 부서번호, 급여 평균을 검색.
+-- 부서 번호별 급여 평균들 중에서 최댓값을 찾는 방법:
+select max(round(avg(salary), 1))
+from employees
+group by department_id;
+
+-- (1)
+select department_id, round(avg(salary), 1) as AVG_SAL
+from employees
+group by department_id
+having avg(salary) = (
+    select max(avg(salary)) from employees
+    group by department_id
+);
+
+-- (2)
+select max(t.avg_salary)
+from (
+    select department_id, avg(salary) as AVG_SALARY 
+    from employees 
+    group by department_id
+) t;
 
 -- 11. 사번, 직원이름, 국가이름, 급여 검색.
+select e.employee_id, e.last_name, c.country_name, e.salary
+from employees e
+    join departments d on e.department_id = d.department_id
+    join locations l on d.location_id = l.location_id
+    join countries c on l.country_id = c.country_id;
 
 -- 12. 국가이름, 국가별 급여 합계 검색.
+select c.country_name, sum(e.salary)
+from employees e
+    join departments d on e.department_id = d.department_id
+    join locations l on d.location_id = l.location_id
+    join countries c on l.country_id = c.country_id
+group by c.country_name;
 
 -- 13. 사번, 직원이름, 직책이름, 급여를 검색.
+
 
 -- 14. 직책이름, 직책별 급여 평균, 최솟값, 최댓값 검색.
 
